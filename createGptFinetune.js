c09fca336bde33ce680a3f1dc3abd279003d0bc7
Tim Restieaux <restieauxbro@hotmail.com>
2022-06-04 15:59:30 +1000

    formatting and finetuning

--- createGptFinetune.js
+++ createGptFinetune.js
@@ -18,31 +18,56 @@ function writeDoc(filename, body) {
   );
 }
 
-function jsonlConversion(arr, { promptField, completion }) {
+function jsonlConversion(arr, { promptFunction, completionFunction }) {
   // the promptField is a column name from the supabase entry, and completion is a function that takes the row as an argument and returns a string
   const jsonl = arr
     .map((row) =>
       JSON.stringify({
-        prompt: promptField ? row[promptField] : "",
-        completion: completion(row) || row,
+        prompt: promptFunction(row) || "",
+        completion: completionFunction(row) || row,
       })
     )
     .join("\n");
   return jsonl;
 }
 
+// Job Profiles for the GPT Finetune
 async function formatData() {
   const data = await readSupabase("internal_job_profiles");
-  const formatted = jsonlConversion(data, {
-    promptField: "title",
-    completion: (row) => {
-      return `WHAT THEY WILL DO:\n\n${row["do"]}\n\nWHAT THEY WILL BE:\n\n${row["be"]}\n\nWHAT THEY WILL HAVE:\n\n${row["have"]}\n\n
-      `;
+  const cleanedData = data
+    .map((row) => {
+      const { title, department, purpose, tasks, be, have } = row;
+      function clean(text) {
+        return text.replace("\\n", "\n").trim();
+      }
+      // clean text in all properties of the row
+      return {
+        title: clean(title),
+        department: clean(department),
+        purpose: clean(purpose),
+        tasks: clean(tasks),
+        be: clean(be),
+        have: clean(have),
+      };
+    })
+    .filter((row) => {
+      // filter out rows that have no tasks or no be or no have
+      return row.tasks && row.be && row.have;
+    });
+  const formatted = jsonlConversion(cleanedData, {
+    promptFunction: ({ title, department, purpose }) => {
+      return `Title: ${title}\nDepartment: ${department}\nPurpose: ${purpose}`;
+    },
+    completionFunction: (row) => {
+      return `### WHAT THEY WILL DO: ###\n\n${row["tasks"]}\n\n### WHAT THEY WILL BE: ###\n\n${row["be"]}\n\n### WHAT THEY WILL HAVE: ###\n\n${row["have"]}
+      `.trim();
     },
   });
   console.log(formatted);
   return formatted;
 }
+
+// ______________________________________________
 
 async function index() {
   try {
--- openAi.js
+++ openAi.js
@@ -32,15 +32,29 @@ async function uploadFile(file, purpose) {
     throw error;
   }
 }
-// uploadFile("./jsonl/website-finetune.jsonl", "fine-tune");
+// uploadFile("./jsonl/job-profiles.jsonl", "fine-tune");
+// returned file-Oo8A9hvERZHgURwxzsFswaGs
 
 async function finetuneJob(uploadedFile) {
-  // purpose can be answers, search, classifications, or fine-tune
   try {
     const response = await openai.createFineTune({
-      training_file: uploadedFile
+      training_file: uploadedFile,
+      suffix: "job-profiles-v1",
     });
-    const data = response;
+    const { data } = response;
+    console.log(data);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+// finetuneJob("file-Oo8A9hvERZHgURwxzsFswaGs");
+// returned curie:ft-restio:job-profiles-v1-2022-06-03-05-43-18
+
+async function listGPTFineTunes() {
+  try {
+    const response = await openai.listFineTunes();
+    const { data } = response.data;
     console.log(data);
     return data;
   } catch (error) {
@@ -48,4 +62,6 @@ async function finetuneJob(uploadedFile) {
   }
 }
 
-finetuneJob('file-Kzb5c8NfDWqJ9KRgyrzwVwTR')
+listGPTFineTunes();
+
+
